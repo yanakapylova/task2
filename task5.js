08ac8519671c9dbaccf8916b26bd024144b41978
yanakapylova <yana.kapylova@innowise.com>
2024-06-18 13:03:54 +0200

    task5

---
+++ task5.js
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/longest-common-prefix/
+
+var longestCommonPrefix = function (strs) {
+  if (strs[0] == "") {
+    return "";
+  }
+  if (strs.length == 1) {
+    return strs[0];
+  }
+
+  let res = "";
+  let i = 0;
+  let flag = true;
+
+  while (flag) {
+    strs.forEach((item) => {
+      if (i != item.length) {
+        if (strs[0][i] != item[i]) {
+          flag = false;
+        }
+      } else {
+        flag = false;
+      }
+    });
+    if (flag) {
+      res = res + strs[0][i];
+    }
+    i++;
+  }
+  return res;
+};
